7194d1b3718a41f3c90693c8f779c0324dcd1543
igorcarmona-git <igorcarmonawork@gmail.com>
2026-06-10 19:10:28 -0300

    fix: update to use onStart state to load data

--- app/src/main/java/com/example/mesa12app/MainActivity.kt
+++ app/src/main/java/com/example/mesa12app/MainActivity.kt
@@ -51,6 +51,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
 
         // Carrega dados iniciais
         loadSavedNames()
